bcc60d306578cde49cd0384104c48b25c21f2823
Matt <6519811+mattgogerly@users.noreply.github.com>
2017-10-09 16:17:49 +0100

    Cleanup

--- src/main/java/com/amazonaws/lambda/holidayfinder/HolidayFinderSpeechletRequestStreamHandler.java
+++ src/main/java/com/amazonaws/lambda/holidayfinder/HolidayFinderSpeechletRequestStreamHandler.java
@@ -1,4 +1,4 @@
-package com.amazonaws.lambda.holidayfinder;
+	package com.amazonaws.lambda.holidayfinder;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -10,7 +10,7 @@ public class HolidayFinderSpeechletRequestStreamHandler extends SpeechletRequest
     
     static {
        supportedApplicationIds = new HashSet<String>();
-       supportedApplicationIds.add("amzn1.ask.skill.8d87cda6-c293-4c24-8e4b-fa2d0d5be3d2");
+       supportedApplicationIds.add("[ID HERE]");
    }
 
     public HolidayFinderSpeechletRequestStreamHandler() {
